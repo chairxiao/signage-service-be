ecd230be3627077998514f5e10fb9d4f3cdeaf09
h257256 <h257256@139.com>
2019-09-27 17:41:35 +0800

    ttt

--- Signage.Service.Infoplay/DownloadService/PlayContent.cs
+++ Signage.Service.Infoplay/DownloadService/PlayContent.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Signage.Service
 {
@@ -58,114 +59,6 @@ namespace Signage.Service
             return rc;
         }
 
-
-        private void SaveText(string title, PlayContent[] textContents)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            foreach (var p in textContents)
-            {
-                if (p.type == ContentType.Text)
-                {
-                    sb.Append(p.content);
-                }
-            }
-            string dfn = Path.Combine(filePlayerRoot, title) + ".txt";
-            TryWriteText(dfn, sb.ToString());
-        }
-
-        private void TryWriteText(string fileName, string content)
-        {
-            int retrys = 0;
-            while (retrys < 1000)
-            {
-                retrys++;
-                try
-                {
-                    File.WriteAllText(fileName, content);
-                    break;
-                }
-                catch (Exception e)
-                {
-
-                }
-                Thread.Sleep(10);
-            }
-        }
-
-        private void SaveFiles(string title, string imageExtend, int MaxImages, PlayContent[] fileContents)
-        {
-            List<string> fileNames = new List<string>();
-
-            foreach (var p in fileContents)
-            {
-                if (p.type == ContentType.File)
-                {
-                    string[] fns = p.content.Split(new char[] { '|' });
-                    fileNames.AddRange(fns);
-
-                }
-            }
-
-            string[] distinctFiles = (from fn in fileNames
-                                      where (File.Exists(fn))
-                                      select fn).Distinct()
-                                    .ToArray();
-
-            int count = distinctFiles.Count();
-            for (int index = 0; index < MaxImages; index++)
-            {
-                string sfn;
-                string dfn = Path.Combine(filePlayerRoot, string.Format("{0}_{1}", title, index));
-                if (count <= 0)
-                {
-                    sfn = "";
-                    dfn = dfn + imageExtend;
-                }
-                else
-                {
-
-                    sfn = distinctFiles[index % count];
-                    dfn = dfn + Path.GetExtension(sfn);
-                }
-                TryCopyFile(sfn, dfn);
-
-            }
-        }
-
-        private void TryCopyFile(string sourceFileName, string destFileName)
-        {
-            int retrys = 0;
-
-            while (retrys < 1000)
-            {
-                retrys++;
-                try
-                {
-                    if (File.Exists(sourceFileName))
-                    {
-                        File.Copy(sourceFileName, destFileName, true);
-                    }
-                    else
-                    {
-                        File.Create(destFileName);
-                    }
-                    break;
-                }
-                catch (IOException e)
-                {
-                    Thread.Sleep(10);
-                }
-            }
-        }
-        private void PurgeFiles(string homeDirectory, string title)
-        {
-            foreach (var pathName in Directory.GetFiles(homeDirectory))
-            {
-                var fileName = Path.GetFileName(pathName);
-            }
-
-        }
 
     }
 }
